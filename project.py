b7e7edc2441d34f677c997b238a310642790e36c
jomarlub17 <161365974+jomarlub17@users.noreply.github.com>
2024-05-18 17:51:51 -0400

    ran and fixed the bugs in this piece of code

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 first_Name = 'Jomar'
 last_Name = 'Lubin'
 # trying out python on github for the first time to see how it works.
-if (first_Name == 'Jomar') and (last_name = 'Lubin'):
+if (first_Name == 'Jomar') and (last_Name == 'Lubin'):
   print(f'Hi you are {first_Name} {last_Name}, you must be the owner of this project.')
 else:
   print(f' You are not the owner of this project.')
